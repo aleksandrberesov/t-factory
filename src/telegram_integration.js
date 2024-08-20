08c652253d9b25c1d2df4f2ae182b37b335808fb
aleksandrberesov <alexandr.beresov@gmail.com>
2024-08-20 11:43:06 -0300

    tg user data

--- src/telegram_integration.js
+++ src/telegram_integration.js
@@ -2,18 +2,18 @@ import { initViewport, retrieveLaunchParams } from '@telegram-apps/sdk';
 
 export async function FullScreen(){
     const [viewport] = initViewport();
-  
+
     const vp = await viewport;
   
     if (!vp.isExpanded) {
         vp.expand(); // will expand the Mini App, if it's not
     }
- }
+}
 
 export function GetUserData(){
     const { initDataRaw, initData } = retrieveLaunchParams();
     const user = initData.user;
     const username = user.username;
-    return username;
+    return initData;
 }
  
