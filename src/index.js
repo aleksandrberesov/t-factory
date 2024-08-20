6fff662a538aa4e101d8c125be213b22b7c31138
aleksandrberesov <alexandr.beresov@gmail.com>
2024-08-20 09:10:33 -0300

    tg user data

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import {FullScreen, GetUserData} from './telegram_integration';
 import * as serviceWorker from './serviceWorker';
-import { GetUserData } from './telegram_integration';
 
 FullScreen();
 alert(GetUserData);
