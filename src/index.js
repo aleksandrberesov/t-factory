8324fb8b418e9b56ae54f6b38fac827f9555c105
aleksandrberesov <alexandr.beresov@gmail.com>
2024-08-23 17:11:43 -0300

    move user info

--- src/index.js
+++ src/index.js
@@ -9,10 +9,5 @@ FullScreen();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 document.getElementById('userinfo').innerText = GetUserData();
-//const s = GetUserData();
-//alert(GetUserData());
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
